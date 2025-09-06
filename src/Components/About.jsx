8366ccfbaedc46fb5836869dbbb1aeb0fe2e63d1
mahirTheCoder <mahirchowdhury518@gmail.com>
2025-09-06 18:44:27 +0600

    about section almost done

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
+import React from "react";
+import aboutImg from "../assets/images/aboutImg.png";
 
 const About = () => {
   return (
     <>
-    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius aut, voluptatum temporibus quaerat, nobis, perspiciatis nihil omnis quia sint inventore ipsa laudantium? Quas ipsum eius aut nobis reprehenderit magnam assumenda?
-    
-    </>
-  )
-}
+      <section id="Section" className="w-full h-[903px] bg-secondary">
+        <div className="container">
+          <div className="aboutRow py-25 flex justify-between">
+            <div className="aboutImage w-full lg:w-[774px] lg:h-[703px]">
+              <img src={aboutImg} alt="img" />
+            </div>
+            <div className="aboutContent mt-[85px]">
+              <h2 className="text-[30px] font-roboto font-semibold text-Brand mb-6">
+                About us{" "}
+              </h2>
+              <h3 className="w-[586px] text-[64px] font-semibold font-roboto text-primary ">The core mission behind all our work</h3>
+            <p className="w-[548px] text-lg font-normal font-dm  text-primary">Lorem ipsum dolor sit amet, consectetur adipiscing elit metus ut tortor purus tincidunt sed lectus ut eros, turpis tincidunt id.</p>
+            <div className="aboutCounter">
+              <div className="counter1">
+                
+              </div>
+            </div>
 
-export default About
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default About;
--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "../Components/Banner";
 import Service from "../Components/Service";
+import About from "../Components/About";
 
 const Home = () => {
   return (
     <>
       <Banner />
       <Service />
+      <About/>
     </>
   );
 };
