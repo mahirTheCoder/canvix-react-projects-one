381fceacb054b9f95934abd929797c09bf8bbde1
mahirTheCoder <mahirchowdhury518@gmail.com>
2025-09-08 00:20:54 +0600

    footer responsive done

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -9,91 +9,91 @@ const Footer = () => {
     <>
       <section id="Footer" className="w-full  bg-secondary  mt-[115px] ">
         <div className="container">
-          <div className="contentPartOne pt-25 flex justify-between flex-wrap pb-25 ">
+          <div className="contentPartOne pt-25 mb-[20px] flex gap-10 lg:gap-0  lg:justify-between flex-wrap pb-10 lg:pb-25 px-3 lg:px-0">
             <div className="logoItems">
-              <img className="w-50 mb-6" src={logo} alt="logoImg" />
-              <p className="lg:w-[357px] w-75 lg:text-lg font-normal font-dm text-primary">
+              <img className="lg:w-50 mb-6" src={logo} alt="logoImg" />
+              <p className="lg:w-[357px] w-75 lg:text-lg  text-xs pb-5  lg:pb-0 bfont-normal font-dm text-primary">
                 We’re a team of strategic creator and digital innovator, united
                 focus in our pursuit of mastery and joyful.
               </p>
             </div>
             <div className="Pages flex flex-col">
-              <h2 className="text-xl font-semibold font-roboto mb-6 text-white ">
+              <h2 className="lg:text-xl text-base font-semibold font-roboto mb-6 text-white ">
                 Pages{" "}
               </h2>
               <a
-                className="text-base font-normal font-dm mb-5 text-primary"
+                className="lg:text-base text-xs font-normal font-dm mb-2 lg:mb-5 text-primary"
                 href="#"
               >
                 Home
               </a>
               <a
-                className="text-base font-normal font-dm mb-5 text-primary"
+                className="lg:text-base text-xs font-normal font-dm mb-2 lg:mb-5 text-primary"
                 href="#"
               >
                 Home 2
               </a>
               <a
-                className="text-base font-normal font-dm mb-5 text-primary"
+                className="lg:text-base text-xs font-normal font-dm mb-2 lg:mb-5 text-primary"
                 href="#"
               >
                 About
               </a>
               <a
-                className="text-base font-normal font-dm mb-5 text-primary"
+                className="lg:text-base text-xs font-normal font-dm mb-2 lg:mb-5 text-primary"
                 href="#"
               >
                 Conatct
               </a>
               <a
-                className="text-base font-normal font-dm mb-5 text-primary"
+                className="lg:text-base text-xs font-normal font-dm mb-2 lg:mb-5 text-primary"
                 href="#"
               >
                 Portfolio
               </a>
               <a
-                className="text-base font-normal font-dm mb-5 text-primary"
+                className="lg:text-base text-xs font-normal font-dm mb-2 lg:mb-5 text-primary"
                 href="#"
               >
                 Portfolio Single
               </a>
             </div>
             <div className="Pages flex flex-col">
-              <h2 className="text-xl font-semibold font-roboto mb-6 text-white ">
+              <h2 className="lg:text-xl text-base font-semibold font-roboto mb-6 text-white">
                 Utility Pages{" "}
               </h2>
               <a
-                className="text-base font-normal font-dm mb-5 text-primary"
+                className="lg:text-base text-xs font-normal font-dm mb-2 lg:mb-5 text-primary"
                 href="#"
               >
                 Style Guide{" "}
               </a>
               <a
-                className="text-base font-normal font-dm mb-5 text-primary"
+                className="lg:text-base text-xs font-normal font-dm mb-2 lg:mb-5 text-primary"
                 href="#"
               >
                 Instruction
               </a>
               <a
-                className="text-base font-normal font-dm mb-5 text-primary"
+                className="lg:text-base text-xs font-normal font-dm mb-2 lg:mb-5 text-primary"
                 href="#"
               >
                 License
               </a>
               <a
-                className="text-base font-normal font-dm mb-5 text-primary"
+                className="lg:text-base text-xs font-normal font-dm mb-2 lg:mb-5 text-primary"
                 href="#"
               >
                 Changelog
               </a>
               <a
-                className="text-base font-normal font-dm mb-5 text-primary"
+                className="lg:text-base text-xs font-normal font-dm mb-2 lg:mb-5 text-primary"
                 href="#"
               >
                 Error 404{" "}
               </a>
               <a
-                className="text-base font-normal font-dm mb-5 text-primary"
+                className="lg:text-base text-xs font-normal font-dm mb-2 lg:mb-5 text-primary"
                 href="#"
               >
                 Password Protected{" "}
@@ -103,41 +103,41 @@ const Footer = () => {
               <h2 className="text-xl font-semibold font-roboto mb-4 text-white ">
                 Subscribe
               </h2>
-              <div className="mail w-[433px]  h-[67px] bg-[#fff] rounded-[30px] flex justify-between items-center ">
+              <div className="mail lg:w-[433px] w-[300px] lg:h-[67px] h-[40px] bg-[#fff] lg:rounded-[30px] flex justify-between items-center rounded-md ">
                 <input
                   className="ml-5 border-none outline-none "
                   type="text"
                   placeholder="Enter your email here"
                 />
-                <p className="w-[118px] h-[47px] bg-secondary text-primary rounded-2xl flex justify-center items-center mr-3">
+                <p className="w-[118px] lg:h-[47px] lg:text-base  text-xs h-8 bg-secondary text-primary rounded-2xl flex justify-center items-center mr-3">
                   Subscribe
                 </p>
               </div>
             </div>
           </div>
-          <hr className="text-primary mb-7" />
-          <div className="footer2ndContent flex  justify-between flex-wrap pb-7">
+          <hr className="text-primary mb-7 "  />
+          <div className="footer2ndContent flex  gap-5 lg:gap-0 px-3 lg:px-0  lg:justify-between flex-wrap pb-7">
             <div className="copyRight">
-              <h2 className="text-xl font-semibold font-roboto mb-4 text-white">
+              <h2 className="lg:text-xl text-base font-semibold font-roboto mb-6 text-white">
                 Copyright by
               </h2>
-              <p className="text-base font-normal font-dm mb-5 text-primary">
+              <p className="lg:text-base text-xs font-normal font-dm mb-2 lg:mb-5 text-primary">
                 Designed by Iconstica.com
               </p>
             </div>
             <div className="copyRight">
-              <h2 className="text-xl font-semibold font-roboto mb-4 text-white">
+              <h2 className="lg:text-xl text-base font-semibold font-roboto mb-6 text-white">
                 Contact Us
               </h2>
-              <p className="text-base font-normal font-dm mb-5 text-primary">
+              <p className="lg:text-base text-xs font-normal font-dm mb-2 lg:mb-5 text-primary">
                 +0 12 457 4578
               </p>
             </div>
             <div className="copyRight">
-              <h2 className="text-xl font-semibold font-roboto mb-4 text-white">
+              <h2 className="lg:text-xl text-base font-semibold font-roboto mb-6 text-white">
                 Address
               </h2>
-              <p className="text-base font-normal font-dm mb-5 text-primary">
+              <p className="lg:text-base text-xs font-normal font-dm mb-2 lg:mb-5 text-primary">
                 119 Tanglewood Lane Gulfport, MS 39503
               </p>
             </div>
