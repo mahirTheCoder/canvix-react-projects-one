4d7934b9ec27618fbbd3cd9fc8577325d59f0c2f
mahirTheCoder <mahirchowdhury518@gmail.com>
2025-09-07 19:11:52 +0600

    show case responsive complete

--- src/Components/Showcase.jsx
+++ src/Components/Showcase.jsx
@@ -10,39 +10,39 @@ import { IoMdArrowDroprightCircle } from "react-icons/io";
 const Showcase = () => {
   return (
     <>
-      <section id="Showcase">
+      <section id="Showcase  ">
         <div className="container">
           <div className="header">
             <CommonHead service2={"Recent Showcase"} />
           </div>
-          <div className="showContent flex flex-wrap justify-between">
+          <div className="showContent flex flex-wrap justify-between px-3 lg:px-0">
             <div className="partone">
               <img
-                className="w-[663px] h-[450px] mb-4 mt-[93px] "
+                className="lg:w-[663px] w-75 lg:h-[450px] lg:mb-4 mb-2 mt-[93px] "
                 src={Showimg}
                 alt="showcaseimg"
               />
-              <h2 className="text-3xl font-semibold font-roboto text-secondary mb-2.5 ">
+              <h2 className="lg:text-3xl text-lg font-semibold font-roboto text-secondary lg:mb-2.5 ">
                 Web UI design
               </h2>
-              <p className="text-lg font-normal font-dm">Creative UI design</p>
-              <div className="two mt-22">
+              <p className="lg:text-lg text-[10px]  font-normal font-dm">Creative UI design</p>
+              <div className="two lg:mt-22">
                 <img
-                  className="w-[663px] h-[450px] mb-4 "
+                  className="lg:w-[663px] w-75 lg:h-[450px] lg:mb-4 mb-2 mt-[93px] "
                   src={Showimg2}
                   alt="showcaseimg"
                 />
-                <h2 className="text-3xl font-semibold font-roboto text-secondary mb-2.5 ">
+                <h2 className="lg:text-3xl text-lg font-semibold font-roboto text-secondary lg:mb-2.5 ">
                   UI design
                 </h2>
-                <p className="text-lg font-normal font-dm">
+                <p className="lg:text-lg text-[10px]  font-normal font-dm">
                   Creative Rebranding for logo
                 </p>
               </div>
             </div>
-            <div className="partTwo mt-[192px]">
-              <div className="buttons flex justify-end mb-[51px]">
-                <div className="btns w-[248px] h-[48px] bg-secondary flex gap-2 justify-center items-center rounded-4xl ">
+            <div className="partTwo mt-15 lg:mt-[192px]">
+              <div className="buttons flex lg:justify-end mb-[51px]">
+                <div className="btns w-[248px] h-[48px] bg-secondary flex gap-2 justify-center items-center lg:rounded-4xl rounded-xl ">
                   <IoMdArrowDroprightCircle className="text-[20px] text-primary" />
                   <p className="text-xl font-normal text-primary font-dm">
                     Start your Free Trial
@@ -51,24 +51,24 @@ const Showcase = () => {
               </div>
               <div className="parttwoImg1">
                  <img
-                className="w-[663px] h-[450px] mb-4 "
+                className="lg:w-[663px] w-75 lg:h-[450px] lg:mb-4 mb-2 lg:mt-[93px] "
                 src={Showimg3}
                 alt="showcaseimg"
               />
-              <h2 className="text-3xl font-semibold font-roboto text-secondary mb-2.5 ">
+              <h2 className="lg:text-3xl text-lg font-semibold font-roboto text-secondary lg:mb-2.5 ">
                 To design Digital Strategy
               </h2>
-              <p className="text-lg font-normal font-dm mb-[93px]">Social Media Marketing</p>
+              <p className="lg:text-lg text-[10px]  font-normal font-dm">Social Media Marketing</p>
               <div className="img2P2">
                    <img
-                  className="w-[663px] h-[450px] mb-4 "
+                  className="lg:w-[663px] w-75 lg:h-[450px] lg:mb-4 mb-2 mt-[93px] "
                   src={Showimg4}
                   alt="showcaseimg"
                 />
-                <h2 className="text-3xl font-semibold font-roboto text-secondary mb-2.5 ">
+                <h2 className="lg:text-3xl text-lg font-semibold font-roboto text-secondary lg:mb-2.5 ">
                   UI design
                 </h2>
-                <p className="text-lg font-normal font-dm">
+                <p className="lg:text-lg text-[10px]  font-normal font-dm">
                   Creative Rebranding for logo
                 </p>
               </div>
