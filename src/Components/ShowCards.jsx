d5e55642667eeaccc0e295f6a9599f8308fba430
mahirTheCoder <mahirchowdhury518@gmail.com>
2025-09-07 12:57:11 +0600

    half complete

---
+++ src/Components/ShowCards.jsx
@@ -0,0 +1,26 @@
+// import React from "react";
+
+// const ShowCards = ({ ShowImg, Showheader, ShowP }) => {
+//   return (
+//     <>
+//       <div className="part1">
+//         <img
+//           className="lg:w-[663px] w-75 lg:h-[450px] mt-21.5"
+//           src={ShowImg}
+//           alt="img"
+//         />
+//         <h2 className="lg:text-[30px] text-lg font-roboto font-semibold text-secondary mt-4.5 mb-2.5 lgpb-22 ">
+//           {Showheader}
+//         </h2>
+//         <p className="lg:text-lg text-xs font-normal font-dm lg:mb-22 mb-5">
+//           {ShowP}
+//         </p>
+//       </div>
+//       <div className="part2">
+      
+//       </div>
+//     </>
+//   );
+// };
+
+// export default ShowCards;
--- src/Components/Showcase.jsx
+++ src/Components/Showcase.jsx
@@ -1,12 +1,47 @@
-import React from 'react'
+import React from "react";
+import CommonHead from "./CommonHead";
+// import ShowCards from "./ShowCards";
+import Showimg from "../assets/images/show1.png";
+import Showimg2 from "../assets/images/show2.png";
 
 const Showcase = () => {
   return (
     <>
-    <h2 className=' text-5xl'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate vitae cumque velit ab nemo ut facilis nostrum iusto iste, assumenda numquam possimus? Aspernatur itaque odit dignissimos recusandae quasi similique iusto!</h2>
-    
+      <section id="Showcase">
+        <div className="container">
+          <div className="header">
+            <CommonHead service2={"Recent Showcase"} />
+          </div>
+          <div className="showContent mt-21.5">
+            <div className="partone">
+              <img
+                className="w-[663px] h-[450px] mb-4 "
+                src={Showimg}
+                alt="showcaseimg"
+              />
+              <h2 className="text-3xl font-semibold font-roboto text-secondary mb-2.5 ">
+                Web UI design
+              </h2>
+              <p className="text-lg font-normal font-dm">Creative UI design</p>
+              <div className="two mt-22">
+                <img
+                  className="w-[663px] h-[450px] mb-4 "
+                  src={Showimg2}
+                  alt="showcaseimg"
+                />
+                <h2 className="text-3xl font-semibold font-roboto text-secondary mb-2.5 ">
+                  UI design
+                </h2>
+                <p className="text-lg font-normal font-dm">
+                  Creative Rebranding for logo
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Showcase
+export default Showcase;
