31655133787b217dfa94f3bfe2740ebd9513359c
mahirTheCoder <mahirchowdhury518@gmail.com>
2025-09-08 23:29:03 +0600

    slider half complete

--- src/Components/SliCanvix.jsx
+++ src/Components/SliCanvix.jsx
@@ -1,73 +1,51 @@
-import React from 'react'
+import React from "react";
 import "slick-carousel/slick/slick.css";
 import Slider from "react-slick";
 import { IoIosArrowBack } from "react-icons/io";
 import { IoIosArrowForward } from "react-icons/io";
-
-
+import sliderImg from "../assets/images/sliderImg.png";
 
 const SliCanvix = () => {
-
-    const settings = {
+  const settings = {
     dots: false,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-        nextArrow: <IoIosArrowForward /> ,
-    prevArrow: <IoIosArrowBack /> 
+    nextArrow: <IoIosArrowForward />,
+    prevArrow: <IoIosArrowBack />,
   };
-
-
 
   return (
     <>
+      <section id="SliderPart" className="mt-26">
+        <div className="container w-full bg-primary h-[482px]">
+          <div className="slider-container">
+            <Slider {...settings}>
+              <div>
+                <div>
+                
+                </div>
+              </div>
+              <div>
+                <h3>2</h3>
+              </div>
+              <div>
+                <h3>3</h3>
+              </div>
+              <div>
+                <h3>4</h3>
+              </div>
+              <div>
+                <h3>5</h3>
+              </div>
+            </Slider>
+          </div>
+        </div>
+      </section>
 
-
-
-    <section>
-        <div className="slider-container">
-      <Slider {...settings}>
-        <div>
-        
-    <div>
-      <img src="" alt="" />
-      <h2></h2>
-      <p></p>
-    </div>
-        </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
-      </Slider>
-    </div>
-    </section>
-
-    <div>
-      <img src="" alt="" />
-      <h2></h2>
-      <p></p>
-    </div>
-    
-    
-    
-    
-    
-    
     </>
-  )
-}
+  );
+};
 
-export default SliCanvix
+export default SliCanvix;
